df539fec18e0f3f3faa3c0c35b91ea953fc4dfde
rrsalian16 <rrsalian16@gmail.com>
2020-09-10 11:41:33 +0530

    url changes

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 export const URL_MAP = {
-    BASE_URL: '',
-    SAMPLE: 'sample',
-    LOGIN: 'login',
+    BASE_URL: 'React-Sample-App',
+    SAMPLE: 'React-Sample-App/sample',
+    LOGIN: 'React-Sample-App/login',
 }
